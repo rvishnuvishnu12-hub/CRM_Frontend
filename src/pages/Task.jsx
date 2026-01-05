8066cef25e705e2b4308d4705a790619f4c791b1
Vasanth775 <vasanthsparrow19@gmail.com>
2026-01-05 14:31:46 +0530

    feat: Update Task component to include placeholder text and improve layout

--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Task = () => {
   return (
-    <div>TASK</div>
+    <div>
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat accusamus a quidem dolor in deserunt minima, aspernatur aperiam excepturi illum dolorem sint. Quas amet eum unde recusandae? Corrupti, id consectetur!
+      <br/>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, voluptatem.
+    </div>
   )
 }
 
